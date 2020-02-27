c05fce6c36747a932c3e71e73c0b6940012b56cd
Loh Eng Liang <lohengliang@gmail.com>
2020-02-27 14:38:57 +0800

    Updates

---
+++ precache-manifest.d2032eca2dff9a875027579325348c53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eabe7685c799027c24ed9db874b897e",
+    "url": "/lennon-wall/index.html"
+  },
+  {
+    "revision": "6e2908f33b36ca662054",
+    "url": "/lennon-wall/static/js/2.94fa38a2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/lennon-wall/static/js/2.94fa38a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4fbbfd34221b8a65f99",
+    "url": "/lennon-wall/static/js/main.41d6f780.chunk.js"
+  },
+  {
+    "revision": "5212b9506a309e62563e",
+    "url": "/lennon-wall/static/js/runtime-main.6b7fb701.js"
+  }
+]);

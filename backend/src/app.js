07e8538d44a7765a533b2d7cdabd3b506bfe9598
Loh Eng Liang <lohengliang@gmail.com>
2020-02-27 13:49:36 +0800

    Added express.static function in app.js in the backend.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import "core-js/stable";
 import cors from "cors";
+import path from "path";
 import "regenerator-runtime/runtime";
 import postRouter from "./routes/postRouter";
 import wallRouter from "./routes/wallRouter";
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => res.send("hit my root endpoint!"));
 
